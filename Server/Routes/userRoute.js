ede29fb2e32a0fa8bf73d8416febc134703aefc4
Abd766 <abdullahmasood766@gmail.com>
2025-04-17 14:41:47 +0530

    Added more API for user

--- Server/Routes/userRoute.js
+++ Server/Routes/userRoute.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const router = express.Router();
 const userController = require("../controller/userController")
-router.get("/users",userController.getUsers);
+
+router.get("/users",userController.getAllUsers);
+router.get("/user/:email",userController.getUser);
+router.post("/adduser",userController.createUser);
+router.put("/updateuser/:email",userController.editUser);
+router.delete("/removeuser/:email",userController.deleteUser);
+
+module.exports=router;
+
 module.exports = router;
